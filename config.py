04f3fc85660025486e082b9c7fd64dd963733158
Githubaswin <67902149+Githubaswin@users.noreply.github.com>
2024-02-04 12:58:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from os import environ
 
 class Config:
-    API_ID = 26229903
-    API_HASH = "b5197d148bd3665a1eb45d1d18b02ad3"
-    BOT_TOKEN = "6737484095:AAHp5AsBUmmnZvZlsaJMkK3kMa4S5A0ahx0"
+    API_ID = 262299031
+    API_HASH = "b5197d148bd3665a1eb45d1d18b02ad31"
+    BOT_TOKEN = "6737484095:AAHp5AsBUmmnZvZlsaJMkK3kMa4S5A0ahx01"
     BOT_SESSION = environ.get("BOT_SESSION", "bot")
-    DATABASE_URI = "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.ldsblpl.mongodb.net/?retryWrites=true&w=majority"
+    DATABASE_URI = "mongodb+srv://Erichdaniken:Erichdaniken@cluster0.ldsblpl.mongodb.net/?retryWrites=true&w=majority1"
     DATABASE_NAME = "Cluster0"
     BOT_OWNER_ID = 503170505
 
