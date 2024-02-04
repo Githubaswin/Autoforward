0f93703a7e86729c0b7cc018191e02fecf965180
Githubaswin <67902149+Githubaswin@users.noreply.github.com>
2024-02-04 11:37:49 +0530

    Add files via upload
    1st Import

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from os import environ 
+
+class Config:
+    API_ID = int(environ.get("API_ID", ))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    DATABASE_URI = environ.get("DB_URI")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    BOT_OWNER_ID = environ.get("BOT_OWNER", "")
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
